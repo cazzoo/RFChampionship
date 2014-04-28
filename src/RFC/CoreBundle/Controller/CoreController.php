b9de6b5e7bea294ebf40babe42325bab54230511
caz <casimir.bonnet@gmail.com>
2014-04-28 21:09:35 +0200

    Création du premier formulaire fonctionnel (games) et mise à jour du projet pour ajouter les sources du projet.

--- src/RFC/CoreBundle/Controller/CoreController.php
+++ src/RFC/CoreBundle/Controller/CoreController.php
@@ -9,26 +9,26 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CoreController extends Controller
 {
-  public function indexAction()
-  {
-    return $this->render('RFCCoreBundle:Core:index.html.twig', array('games' => array()));
-  }
-    
-  public function accessGameAction($gameId)
-  {
-    //return $this->render('RFCCoreBundle:Core:gameIndex.html.twig', array('gameId' => $gameId));
-    return new Response('Affichage du jeu ' . $gameId);
-  }
-public function menuAction($number)
-{
-    $gameList = array(
-      array('id' => 1, 'name' => 'Jeu 1'),
-      array('id' => 2, 'name' => 'Jeu 2'),
-      array('id' => 3, 'name' => 'Jeu 3')
-    );
-    
-    return $this->render('RFCCoreBundle:Core:menu.html.twig', array(
-      'gameList' => $gameList
-    ));
-}
+    public function indexAction()
+    {
+        return $this->render('RFCCoreBundle:Core:index.html.twig', array('games' => array()));
+    }
+
+    public function accessGameAction($gameId)
+    {
+        return $this->render('RFCCoreBundle:Core:gameIndex.html.twig', array('gameId' => $gameId));
+    }
+
+    public function menuAction($number)
+    {
+        $gameList = array(
+            array('id' => 1, 'name' => 'Jeu 1'),
+            array('id' => 2, 'name' => 'Jeu 2'),
+            array('id' => 3, 'name' => 'Jeu 3')
+        );
+
+        return $this->render('RFCCoreBundle:Core:menu.html.twig', array(
+            'gameList' => $gameList
+        ));
+    }
 }
