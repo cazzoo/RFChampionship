a81117d163c78aa4d73ccd7381d5135622eead27
caz <casimir.bonnet@gmail.com>
2014-04-30 08:05:00 +0200

    Création d'un bundle Admin
    Création et déplacement des controllers/vues actuelles d'administration
    dans un Bundle spécifique à l'administration (RFCAdminBundle).

---
+++ src/RFC/AdminBundle/Controller/RuleController.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace RFC\AdminBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use RFC\CoreBundle\Entity\Rule;
+use RFC\CoreBundle\Form\RuleType;
+
+/**
+ * Rule controller.
+ *
+ */
+class RuleController extends Controller
+{
+
+    /**
+     * Lists all Rule entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('RFCCoreBundle:Rule')->findAll();
+
+        return $this->render('RFCAdminBundle:Rule:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new Rule entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Rule();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_rule_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('RFCAdminBundle:Rule:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to create a Rule entity.
+    *
+    * @param Rule $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createCreateForm(Rule $entity)
+    {
+        $form = $this->createForm(new RuleType(), $entity, array(
+            'action' => $this->generateUrl('admin_rule_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Rule entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Rule();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('RFCAdminBundle:Rule:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Rule entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('RFCCoreBundle:Rule')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Rule entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('RFCAdminBundle:Rule:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Rule entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('RFCCoreBundle:Rule')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Rule entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('RFCAdminBundle:Rule:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Rule entity.
+    *
+    * @param Rule $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Rule $entity)
+    {
+        $form = $this->createForm(new RuleType(), $entity, array(
+            'action' => $this->generateUrl('admin_rule_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Rule entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('RFCCoreBundle:Rule')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Rule entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_rule_edit', array('id' => $id)));
+        }
+
+        return $this->render('RFCAdminBundle:Rule:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Rule entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('RFCCoreBundle:Rule')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Rule entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin_rule'));
+    }
+
+    /**
+     * Creates a form to delete a Rule entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_rule_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
---
+++ src/RFC/AdminBundle/Controller/SessionController.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace RFC\AdminBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use RFC\CoreBundle\Entity\Session;
+use RFC\CoreBundle\Form\SessionType;
+
+/**
+ * Session controller.
+ *
+ */
+class SessionController extends Controller
+{
+
+    /**
+     * Lists all Session entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('RFCCoreBundle:Session')->findAll();
+
+        return $this->render('RFCAdminBundle:Session:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new Session entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Session();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_session_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('RFCAdminBundle:Session:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to create a Session entity.
+    *
+    * @param Session $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createCreateForm(Session $entity)
+    {
+        $form = $this->createForm(new SessionType(), $entity, array(
+            'action' => $this->generateUrl('admin_session_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Session entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Session();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('RFCAdminBundle:Session:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Session entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('RFCCoreBundle:Session')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Session entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('RFCAdminBundle:Session:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Session entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('RFCCoreBundle:Session')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Session entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('RFCAdminBundle:Session:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Session entity.
+    *
+    * @param Session $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Session $entity)
+    {
+        $form = $this->createForm(new SessionType(), $entity, array(
+            'action' => $this->generateUrl('admin_session_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Session entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('RFCCoreBundle:Session')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Session entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_session_edit', array('id' => $id)));
+        }
+
+        return $this->render('RFCAdminBundle:Session:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Session entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('RFCCoreBundle:Session')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Session entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin_session'));
+    }
+
+    /**
+     * Creates a form to delete a Session entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_session_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
