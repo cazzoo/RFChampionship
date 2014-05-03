259db157625170869d3ea6cb0ee433b54467e9c9
caz <casimir.bonnet@gmail.com>
2014-05-04 00:07:44 +0200

    Introducing Access object + User forms
    Mise à jour des formulaires d'admin pour utilisateurs

---
+++ src/RFC/UserBundle/Entity/Access.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace RFC\UserBundle\Entity;
+
+/**
+ * Access
+ */
+class Access
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $role;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set role
+     *
+     * @param string $role
+     * @return Access
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return string 
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+}
