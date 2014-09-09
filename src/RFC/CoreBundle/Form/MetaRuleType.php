dbe223c22d017d0f6980cb9ef7cb92ec60766728
caz <casimir.bonnet@gmail.com>
2014-09-09 22:33:43 +0200

    Form correction
    Enable Rules & MetaRules creation

--- src/RFC/CoreBundle/Form/MetaRuleType.php
+++ src/RFC/CoreBundle/Form/MetaRuleType.php
@@ -1,20 +1,21 @@
 <?php
-/*  //RF//Championship is a multi-racing game team manager that allows members to organize and follow championships.
-    Copyright (C) 2014 - //Racing-France//
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
-
+/*
+ * //RF//Championship is a multi-racing game team manager that allows members to organize and follow championships.
+ * Copyright (C) 2014 - //Racing-France//
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace RFC\CoreBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
@@ -23,75 +24,64 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use RFC\CoreBundle\Form\DataTransformer\GameToIntTransformer;
 use RFC\CoreBundle\Entity\RuleRepository;
 
-class MetaRuleType extends AbstractType
-{
-
-    public function __construct($id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     *
-     * @param FormBuilderInterface $builder            
-     * @param array $options            
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $id = $this->id;
-        $gameTransformer = new GameToIntTransformer($options['em']);
-        
-        $builder->add('name')
-            ->add('description', 'textarea')
-            ->add('isAgreed', 'checkbox', array(
-            'required' => false
-        ))
-            ->add('listRules', 'entity', array(
-            'multiple' => true,
-            'required' => false,
-            'class' => 'RFCCoreBundle:Rule',
-            'query_builder' => function (RuleRepository $er) use($id)
-            {
-                return $er->createQueryBuilder('r')
-                    ->where('r.game = :id')
-                    ->setParameter('id', $id);
-            }
-        ))
-            ->add('commentsActive', 'checkbox', array(
-            'required' => false
-        ))
+class MetaRuleType extends AbstractType {
+	public function __construct($id) {
+		$this->id = $id;
+	}
+	
+	/**
+	 *
+	 * @param FormBuilderInterface $builder        	
+	 * @param array $options        	
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$id = $this->id;
+		$gameTransformer = new GameToIntTransformer ( $options ['em'] );
+		
+		$builder->add ( 'name' )->add ( 'description', 'textarea', array (
+				'required' => false 
+		) )->add ( 'isAgreed', 'checkbox', array (
+				'required' => false 
+		) )->add ( 'listRules', 'entity', array (
+				'multiple' => true,
+				'required' => false,
+				'class' => 'RFCCoreBundle:Rule',
+				'query_builder' => function (RuleRepository $er) use($id) {
+					return $er->createQueryBuilder ( 'r' )->where ( 'r.game = :id' )->setParameter ( 'id', $id );
+				} 
+		) )->add ( 'commentsActive', 'checkbox', array (
+				'required' => false 
+		) )
             /*->add($builder->create('game', 'hidden')
             ->addModelTransformer($gameTransformer))*/
-            ->add('game', 'entity', array(
-            'class' => 'RFC\CoreBundle\Entity\Game'
-        ));
-    }
-
-    /**
-     *
-     * @param OptionsResolverInterface $resolver            
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'RFC\CoreBundle\Entity\MetaRule'
-        ));
-        
-        $resolver->setRequired(array(
-            'em'
-        ));
-        
-        $resolver->setAllowedTypes(array(
-            'em' => 'Doctrine\Common\Persistence\ObjectManager'
-        ));
-    }
-
-    /**
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return 'rfc_corebundle_metarule';
-    }
+            ->add ( 'game', 'entity', array (
+				'class' => 'RFC\CoreBundle\Entity\Game' 
+		) );
+	}
+	
+	/**
+	 *
+	 * @param OptionsResolverInterface $resolver        	
+	 */
+	public function setDefaultOptions(OptionsResolverInterface $resolver) {
+		$resolver->setDefaults ( array (
+				'data_class' => 'RFC\CoreBundle\Entity\MetaRule' 
+		) );
+		
+		$resolver->setRequired ( array (
+				'em' 
+		) );
+		
+		$resolver->setAllowedTypes ( array (
+				'em' => 'Doctrine\Common\Persistence\ObjectManager' 
+		) );
+	}
+	
+	/**
+	 *
+	 * @return string
+	 */
+	public function getName() {
+		return 'rfc_corebundle_metarule';
+	}
 }
--- src/RFC/CoreBundle/Form/RuleType.php
+++ src/RFC/CoreBundle/Form/RuleType.php
@@ -1,20 +1,21 @@
 <?php
-/*  //RF//Championship is a multi-racing game team manager that allows members to organize and follow championships.
-    Copyright (C) 2014 - //Racing-France//
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
-
+/*
+ * //RF//Championship is a multi-racing game team manager that allows members to organize and follow championships.
+ * Copyright (C) 2014 - //Racing-France//
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace RFC\CoreBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
@@ -22,69 +23,59 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use RFC\CoreBundle\Entity\TypeSessionRepository;
 
-class RuleType extends AbstractType
-{
-
-    public function __construct($id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     *
-     * @param FormBuilderInterface $builder            
-     * @param array $options            
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $id = $this->id;
-        
-        $builder->add('name')
-            ->add('description', 'textarea')
-            ->add('value')
-            ->add('typeSession', null, array(
-            'required' => true,
-            'class' => 'RFCCoreBundle:TypeSession',
-            'query_builder' => function (TypeSessionRepository $er) use($id)
-            {
-                return $er->createQueryBuilder('t')
-                    ->where('t.game = :id')
-                    ->setParameter('id', $id);
-            }
-        ))
-            ->add('commentsActive', 'checkbox', array(
-            'required' => false
-        ))
-            ->add('game', 'entity', array(
-            'class' => 'RFC\CoreBundle\Entity\Game'
-        ));
-    }
-
-    /**
-     *
-     * @param OptionsResolverInterface $resolver            
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'RFC\CoreBundle\Entity\Rule'
-        ));
-        
-        $resolver->setRequired(array(
-            'em'
-        ));
-        
-        $resolver->setAllowedTypes(array(
-            'em' => 'Doctrine\Common\Persistence\ObjectManager'
-        ));
-    }
-
-    /**
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return 'rfc_corebundle_rule';
-    }
+class RuleType extends AbstractType {
+	public function __construct($id) {
+		$this->id = $id;
+	}
+	
+	/**
+	 *
+	 * @param FormBuilderInterface $builder        	
+	 * @param array $options        	
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$id = $this->id;
+		
+		$builder->add ( 'name' )->add ( 'description', 'textarea', array (
+				'required' => false 
+		) )->add ( 'value', 'text', array (
+				'required' => false 
+		) )->add ( 'typeSession', null, array (
+				'required' => true,
+				'class' => 'RFCCoreBundle:TypeSession',
+				'query_builder' => function (TypeSessionRepository $er) use($id) {
+					return $er->createQueryBuilder ( 't' )->where ( 't.game = :id' )->setParameter ( 'id', $id );
+				} 
+		) )->add ( 'commentsActive', 'checkbox', array (
+				'required' => false 
+		) )->add ( 'game', 'entity', array (
+				'class' => 'RFC\CoreBundle\Entity\Game' 
+		) );
+	}
+	
+	/**
+	 *
+	 * @param OptionsResolverInterface $resolver        	
+	 */
+	public function setDefaultOptions(OptionsResolverInterface $resolver) {
+		$resolver->setDefaults ( array (
+				'data_class' => 'RFC\CoreBundle\Entity\Rule' 
+		) );
+		
+		$resolver->setRequired ( array (
+				'em' 
+		) );
+		
+		$resolver->setAllowedTypes ( array (
+				'em' => 'Doctrine\Common\Persistence\ObjectManager' 
+		) );
+	}
+	
+	/**
+	 *
+	 * @return string
+	 */
+	public function getName() {
+		return 'rfc_corebundle_rule';
+	}
 }
